14528f533538b51608500058275698b178b255c4
Leandro Guerrero <leandromartin747@gmail.com>
2024-06-27 19:59:24 -0500

    Ejercicios java

--- MiPrimerPrograma.java
+++
@@ -1,8 +0,0 @@
-public class MiPrimerPrograma {
-   public static void main(String[] args) {
-      System.out.println("5 + 5 = "+(5+5));
-      System.out.println("5 % 5 = "+(5%5));
-      System.out.println("7 / 5 = "+(1d/3d));
-      System.out.println("7 / 5 = "+(1F/3F));
-   }
-}
--- TiposDeDatos.java
+++
@@ -1,8 +0,0 @@
-public class TiposDeDatos {
-   public static void main(String[] args) {
-   String nombre;
-   nombre = "Leandro";
-   int miEdad = 26;
-   System.out.println("Mi nombre es " + nombre + ", y tengo " + miEdad +" años.");
-   }
-}
